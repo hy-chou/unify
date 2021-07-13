faf72312de38260cf153d0fdc535918d91681d34
Hsuan-Yu Chou <55382796+hy-chou@users.noreply.github.com>
2021-07-13 16:16:53 +0800

    mod style

--- src/Game.js
+++ src/Game.js
@@ -1,14 +1,15 @@
-import React, {useState} from 'react';
-import Member from './Member';
+import React, { useState } from "react";
+import Member from "./Member";
 
-const lvl = 7
-const lvll = []
+const style = Math.random();
+const lvl = 7;
+const lvll = [];
 for (let i = 0; i < lvl; i++) {
-  lvll.push(i)
+  lvll.push(i);
 }
 
 function united(queue) {
-  let firstQ = queue[0]
+  let firstQ = queue[0];
   for (let i = 1; i < lvl; i++) {
     if (firstQ !== queue[i]) {
       return false;
@@ -17,15 +18,15 @@ function united(queue) {
   return true;
 }
 
-function newQ(fill=null) {
-  let queue = []
+function newQ(fill = null) {
+  let queue = [];
   while (queue === [] || united(queue)) {
-    queue = []
-    for (let i = 0; i < lvl; i++){
-      queue.push(fill || Math.floor(Math.random() * 2))
+    queue = [];
+    for (let i = 0; i < lvl; i++) {
+      queue.push(fill || Math.floor(Math.random() * 2));
     }
   }
-  return queue
+  return queue;
 }
 
 function Game(props) {
@@ -35,25 +36,33 @@ function Game(props) {
     let nowQ = queue.slice();
 
     nowQ[i] = nowQ[i] ? 0 : 1;
-    if (i - 1 >= 0) {nowQ[i - 1] = nowQ[i - 1] ? 0 : 1;}
-    if (i + 1 < lvl) {nowQ[i + 1] = nowQ[i + 1] ? 0 : 1;}
+    if (i - 1 >= 0) {
+      nowQ[i - 1] = nowQ[i - 1] ? 0 : 1;
+    }
+    if (i + 1 < lvl) {
+      nowQ[i + 1] = nowQ[i + 1] ? 0 : 1;
+    }
     setQ(nowQ);
-  }
+  };
 
   if (united(queue)) {
-    console.log("1");
-    setTimeout(()=>{console.log("3");setQ(newQ());}, 3000)
-    console.log("2");
+    setTimeout(() => {
+      setQ(newQ());
+    }, 3000);
   }
+
   return (
-    <div className="queue flex-container">
-      {lvll.map((id, value)=>
+    <div className="queue container">
+      {lvll.map((id, value) => (
         <Member
           key={id}
           value={queue[value]}
-          onClick={()=> handleClick(value)} />)}
+          style={style}
+          onClick={() => handleClick(value)}
+        />
+      ))}
     </div>
   );
 }
 
-export default Game
+export default Game;
--- src/Member.js
+++ src/Member.js
@@ -1,11 +1,14 @@
-import React from 'react';
+import React from "react";
 
 function Member(props) {
+  const d0 = props.style < 0.5 ? "0" : "#";
+  const d1 = props.style < 0.5 ? "1" : "-";
+
   return (
-    <div className="member flex-item" onClick={props.onClick}>
-      {props.value}
+    <div className="member item" onClick={props.onClick}>
+      {props.value ? d1 : d0}
     </div>
   );
 }
 
-export default Member
+export default Member;
